d886f2d102ffb1a0ce868e31665fbe5e1efbc3bd
dejavuurbe <dejavuurbe@gmail.com>
2026-09-23 10:25:15 -0300

    Configura base para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-const site = process.env.SITE_URL || 'https://example.com';
+const site = process.env.SITE_URL || 'https://dejavuurbe.github.io/pierre-menard-web/';
 
 export default defineConfig({
   site,
+  base: '/pierre-menard-web',
   output: 'static',
   integrations: [sitemap()],
 });
